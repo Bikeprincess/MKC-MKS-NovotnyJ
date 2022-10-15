98ff4fa60d7b7e79cfaf1bb749ea7368ca11c6c1
NovotnyJ <jan.781@seznam.cz>
2022-10-15 10:09:39 +0200

    2.4 init point

---
+++ Cv03_b/Core/Inc/sct.h
@@ -0,0 +1,27 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 14, 2022
+ *      Author: NovotnyJ
+ */
+
+//Pin definition for sct
+//SDI on D5 - PB4
+//CLK on D3 - PB3
+//LA on D4 - PB5
+//OE on D6 - PB10
+
+#include <stdint.h>
+#include "main.h"
+
+#ifndef SCT_H_
+#define SCT_H_
+
+
+
+void sct_init(void);
+void sct_led(const uint32_t aVal);
+void sct_value(const uint32_t aVal);
+
+
+#endif /* SCT_H_ */
---
+++ Cv03_b/Core/Src/sct.c
@@ -0,0 +1,73 @@
+#include "sct.h"
+
+static const uint32_t gDisplayRegValues[3][10] = {
+{
+//PCDE--------GFAB @ DIS1
+0b0111000000000111 << 16,
+0b0100000000000001 << 16,
+0b0011000000001011 << 16,
+0b0110000000001011 << 16,
+0b0100000000001101 << 16,
+0b0110000000001110 << 16,
+0b0111000000001110 << 16,
+0b0100000000000011 << 16,
+0b0111000000001111 << 16,
+0b0110000000001111 << 16,
+},
+{
+//----PCDEGFAB---- @ DIS2
+0b0000011101110000 << 0,
+0b0000010000010000 << 0,
+0b0000001110110000 << 0,
+0b0000011010110000 << 0,
+0b0000010011010000 << 0,
+0b0000011011100000 << 0,
+0b0000011111100000 << 0,
+0b0000010000110000 << 0,
+0b0000011111110000 << 0,
+0b0000011011110000 << 0,
+},
+{
+//PCDE--------GFAB @ DIS3
+0b0111000000000111 << 0,
+0b0100000000000001 << 0,
+0b0011000000001011 << 0,
+0b0110000000001011 << 0,
+0b0100000000001101 << 0,
+0b0110000000001110 << 0,
+0b0111000000001110 << 0,
+0b0100000000000011 << 0,
+0b0111000000001111 << 0,
+0b0110000000001111 << 0,
+},
+};
+
+void sct_init(void)
+{
+	sct_led(0);
+	return;
+}
+
+void sct_led(const uint32_t aVal)
+{
+	uint32_t iVal = aVal;
+	for (volatile uint32_t i = 0; i < 32; i++)
+	{
+		HAL_GPIO_WritePin(SCT_SDI_GPIO_Port, SCT_SDI_Pin, iVal & 0x01);
+		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin,1);
+		iVal >>= 1;
+		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin,0);
+	}
+	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_SDI_Pin,1);
+	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_SDI_Pin,0);
+	return;
+}
+
+void sct_value(const uint32_t aVal)
+{
+	uint32_t iVal = aVal;
+	//while (iVal > 999) iVal /= 10;//check the input number for max value 999 and do div by 10 to number <= 999
+	uint32_t iLedConst = gDisplayRegValues[1][(iVal/100)%10] | gDisplayRegValues[1][(iVal/10)%10] | gDisplayRegValues[1][iVal%10];
+	sct_led(iLedConst);
+	return;
+}
